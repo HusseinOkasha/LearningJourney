937e4ec4e65ed2f9f62bc8e45bbb5ea5b7bc711c
HusseinOkasha <husseinokasha13@gmail.com>
2024-08-07 19:43:51 +0300

    create student entity.
    - create basic entity called student.
    - it has firstname, lastname, email ,and age.
    - create a named unique constaint which on email.
    - use JpaRespository to avoid rewritting boilerplate code.

--- project1/src/main/java/com/project1/Project1Application.java
+++ project1/src/main/java/com/project1/Project1Application.java
@@ -1,7 +1,9 @@
 package com.project1;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Project1Application {
@@ -10,4 +12,11 @@ public class Project1Application {
 		SpringApplication.run(Project1Application.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+		return  args -> {
+			Student student = new Student("Hussein", "Okasha", "email@gmail.com", 21);
+			studentRepository.save(student);
+		};
+	}
 }
