4c863042b7b770448567bec4bbc05f3e1d1b7ac5
HusseinOkasha <husseinokasha13@gmail.com>
2024-08-19 11:59:07 +0300

    refactor: create CreateUserProfileComponent
    - provide better feedback for the user.
    - enhance handling the state.
    - disable the submit button on while waiting for server response.

--- project4/src/main/frontend/src/components/CreateUserProfileComponent.tsx
+++ project4/src/main/frontend/src/components/CreateUserProfileComponent.tsx
@@ -1,59 +1,88 @@
 import axios from "axios";
-import { useRef, useState } from "react";
-import UserProfile from "./UserProfile";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const CreateUserProfileComponent = () => {
   // states
-  const [isLoadingState, setIsLoadingState] = useState<boolean>();
-  const [successMessage, setSuccessMessage] = useState<string>();
-  const [errorMessage, setErrorMessage] = useState<string>();
-  //  reference for username.
-  const usernameRef: React.RefObject<HTMLInputElement> = useRef(null);
+  const [formState, setFormState] = useState({
+    isLoading: false,
+    successMessage: "",
+    errorMessage: "",
+  });
+
+  const [username, setUsername] = useState("");
 
   const navigate = useNavigate();
 
   async function onSubmitHandler(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    // get username value
-    const username: string = usernameRef.current?.value;
-
     // construct the url
-    const url: string = "http://localhost:8080/api/profile";
+    const url: string = "http://localhost:8080/api/profile/";
 
-    setIsLoadingState(true);
-    const response = await axios.post(
-      url,
-      { username: username },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
+    // update the form state.
+    setFormState({
+      ...formState,
+      isLoading: true,
+      errorMessage: "",
+      successMessage: "",
+    });
+
+    try {
+      // send the request.
+      const response = await axios.post(
+        url,
+        { username: username },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      // update the form state.
+      setFormState({ ...formState, isLoading: false });
+      if (response.status === 200) {
+        setFormState({
+          ...formState,
+          successMessage: "User profile created successfully",
+        });
+
+        // store the userProfileId in the local storage
+        localStorage.setItem("userProfileId", response.data.id);
+        // navigate to /profile url
+        navigate("/profile", { replace: true });
+      } else {
+        console.log(response);
+        setFormState({
+          ...formState,
+          errorMessage: "failed to create the profile.",
+        });
       }
-    );
-    setIsLoadingState(false);
-    if (response.status == 200) {
-      setSuccessMessage("profile created succesfully");
-      localStorage.setItem("userProfileId", response.data.id);
-      console.log(localStorage.getItem("userProfileId"));
-      navigate("/profile", { replace: true });
-    } else {
-      setErrorMessage("Failed to create profile");
+    } catch (error) {
+      setFormState({
+        ...formState,
+        errorMessage: "failed to create user profile",
+      });
     }
   }
 
   return (
     <div className="container ">
       <div className="row justify-content-center">
-        {successMessage && (
-          <div className="alert alert-success"> {successMessage}</div>
-        )}
-        {errorMessage && (
-          <div className="alert alert-danger"> {errorMessage}</div>
-        )}
         <div className="col-xl-6 col-md-8 align-self-center">
           <div className="container-sm justify-content-center mt-5">
+            {formState.successMessage && (
+              <div className="alert alert-success">
+                {" "}
+                {formState.successMessage}
+              </div>
+            )}
+            {formState.errorMessage && (
+              <div className="alert alert-danger">
+                {" "}
+                {formState.errorMessage}
+              </div>
+            )}
             <div className="row">
               <div className="col align-self-center">
                 <h1 className="text-center"> Create Profile </h1>
@@ -70,7 +99,7 @@ const CreateUserProfileComponent = () => {
                   className="form-control"
                   id="username"
                   placeholder="user name"
-                  ref={usernameRef}
+                  onChange={(e) => setUsername(e.target.value)}
                 />
                 <button className="btn btn-secondary col-3 mt-2 align-self-center">
                   submit
